3a0bed02bb9cb17169251d43f8767cc4329a7fa9
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2021-12-19 19:02:57 +0100

    RED: Refactor the name class and state the real use case

--- src/test/java/RomanNumeralsTest.java
+++ src/test/java/RomanNumeralsTest.java
@@ -2,11 +2,11 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ClassTest {
+public class RomanNumeralsTest {
 
 	@Test
-	public void renameMe() {
-		assertTrue(true);
+	public void given_a_1_should_return_a_I() {
+    assertTrue(RomanNumerals.convert(1).equals("I"));
 	}
 
 }
