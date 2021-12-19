f05baf28e67bc5305a3badd27b3deaff46cf19fa
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2021-12-19 19:08:43 +0100

    Refactor: change the assertion on the given_a_1_should_return_a_I to be more explicit

--- src/test/java/RomanNumeralsTest.java
+++ src/test/java/RomanNumeralsTest.java
@@ -1,12 +1,13 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RomanNumeralsTest {
 
 	@Test
 	public void given_a_1_should_return_a_I() {
-    assertTrue(RomanNumerals.convert(1).equals("I"));
+    assertEquals("I", RomanNumerals.convert(1));
 	}
 
 }
