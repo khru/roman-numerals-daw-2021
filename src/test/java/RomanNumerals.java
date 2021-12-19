98f156148679667696ca3f408f2bb7bba5e86c60
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2021-12-19 20:10:14 +0100

    Green: fixing number comparation on the 9 given_a_9_should_return_a_IX

--- src/test/java/RomanNumerals.java
+++ src/test/java/RomanNumerals.java
@@ -2,7 +2,7 @@ public class RomanNumerals {
   public static String convert(int number) {
     StringBuilder result = new StringBuilder();
 
-    if (number >= 9) {
+    if (number == 9) {
       return "IX";
     }
     if (number >= 5) {
