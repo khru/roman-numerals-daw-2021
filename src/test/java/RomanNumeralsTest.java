8458dd48e5b6a3e42c4c2a101b2c4a2ed0c6be5d
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2021-12-19 22:29:14 +0100

    Refactor: update and add new dependencies as well as update all the tests

--- src/test/java/RomanNumeralsTest.java
+++ src/test/java/RomanNumeralsTest.java
@@ -1,83 +1,49 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RomanNumeralsTest {
 
-	@Test
-	public void given_a_1_should_return_a_I() {
-    assertEquals("I", RomanNumerals.convert(1));
-	}
-
-  @Test
-  public void given_a_2_should_return_a_II() {
-    assertEquals("II", RomanNumerals.convert(2));
+  @ParameterizedTest
+  @CsvSource({
+    "1,I",
+    "5,V",
+    "10,X"
+  })
+  public void given_a_unit_number_should_convert_it(int number, String expected) {
+    assertEquals(expected, RomanNumerals.convert(number));
   }
 
-  @Test
-  public void given_a_3_should_return_a_III() {
-    assertEquals("III", RomanNumerals.convert(3));
+  @ParameterizedTest
+  @CsvSource({
+    "4,IV",
+    "9,IX",
+    "14,XIV",
+    "15,XV",
+    "19,XIX",
+  })
+  public void given_a_subtracted_unit_should_convert_it(int number, String expected) {
+    assertEquals(expected, RomanNumerals.convert(number));
   }
 
-  @Test
-  public void given_a_4_should_return_a_IV() {
-    assertEquals("IV", RomanNumerals.convert(4));
-  }
-
-  @Test
-  public void given_a_5_should_return_a_V() {
-    assertEquals("V", RomanNumerals.convert(5));
-  }
-
-  @Test
-  public void given_a_6_should_return_a_VI() {
-    assertEquals("VI", RomanNumerals.convert(6));
-  }
-
-  @Test
-  public void given_a_8_should_return_a_VIII() {
-    assertEquals("VIII", RomanNumerals.convert(8));
-  }
-
-  @Test
-  public void given_a_9_should_return_a_IX() {
-    assertEquals("IX", RomanNumerals.convert(9));
-  }
-
-  @Test
-  public void given_a_10_should_return_a_X() {
-    assertEquals("X", RomanNumerals.convert(10));
-  }
-
-  @Test
-  public void given_a_11_should_return_a_XI() {
-    assertEquals("XI", RomanNumerals.convert(11));
-  }
-
-  @Test
-  public void given_a_13_should_return_a_XIII() {
-    assertEquals("XIII", RomanNumerals.convert(13));
-  }
-
-  @Test
-  public void given_a_14_should_return_a_XIV() {
-    assertEquals("XIV", RomanNumerals.convert(14));
-  }
-
-  @Test
-  public void given_a_15_should_return_a_XV() {
-    assertEquals("XV", RomanNumerals.convert(15));
-  }
-
-  @Test
-  public void given_a_19_should_return_a_XIX() {
-    assertEquals("XIX", RomanNumerals.convert(19));
-  }
-
-  @Test
-  public void given_a_20_should_return_a_XX() {
-    assertEquals("XX", RomanNumerals.convert(20));
+  @ParameterizedTest
+  @CsvSource({
+    "2,II",
+    "3,III",
+    "6,VI",
+    "7,VII",
+    "8,VIII",
+    "11,XI",
+    "12,XII",
+    "13,XIII",
+    "16,XVI",
+    "17,XVII",
+    "18,XVIII",
+    "20,XX",
+  })
+  public void given_a_number_should_return_a_repeatable_number(int number, String expected) {
+    assertEquals(expected, RomanNumerals.convert(number));
   }
 
 }
