e01a4e3fec91e3b8771cfc390eec7e239c75a3cd
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2021-12-19 20:08:39 +0100

    Green: making small changes to make the given_a_9_should_return_a_IX use case pass

--- src/test/java/RomanNumerals.java
+++ src/test/java/RomanNumerals.java
@@ -1,6 +1,10 @@
 public class RomanNumerals {
   public static String convert(int number) {
     StringBuilder result = new StringBuilder();
+
+    if (number >= 9) {
+      return "IX";
+    }
     if (number >= 5) {
       result.append("V");
       number -= 5;
